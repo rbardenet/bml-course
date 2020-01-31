cdf3c76f617d833749cb9533ea05d26a40deb844
Rémi Bardenet <remi.bardenet@gmail.com>
2020-01-31 16:46:17 +0100

    added complete solution to nonparametric regression

--- practicals/nonparametric_regression/nonparametric_regression_student.py
+++ practicals/nonparametric_regression/nonparametric_regression_student.py
@@ -27,7 +27,7 @@ class BayesianOptimization:
         self.sample_size = X.shape[0]
         self.dimension = X.shape[1]
 
-        # Uncomment the following two lines and specify a kernel to specify the GP
+        # Specify the GP
         kernel = 1.0 * Matern(length_scale=1.0, length_scale_bounds=(1e-1, 10.0),
             nu=1.5) + 1.0 * WhiteKernel(noise_level=.1)
         self.gp = sklgp.GaussianProcessRegressor(kernel = kernel, normalize_y=True)
@@ -44,24 +44,41 @@ class BayesianOptimization:
         return self.gp.sample_y(X_test, 1, random_state=random_state)
 
     def acquisition_criterion(self, X_test):
-        """implement your choice of acquisition criterion, say EI.
-        Return a (1,len(X_test))-array.
+        """implement your choice of acquisition criterion, say EI
         """
-        return
+        m = np.min(self.y) # current minimum value
+        y_mean, y_std = self.predict(X_test)
+        u = (m - y_mean)/y_std
+        ei = y_std * (u*sps.norm.cdf(u) + sps.norm.pdf(u))
+        return ei
 
     def find_next_point(self, number_of_restarts):
-        """maximize acquisition criterion. Return a (1,1)-array
-        containing the optimal value of x.
+        """maximize acquisition criterion
         """
-        return
+        # Set things up
+        criterion = lambda x: -self.acquisition_criterion(
+            x.reshape((self.dimension,1))).flatten()[0]
+        bounds = [(0,1)]*self.dimension
+        number_of_restarts = 5
+
+        # Optimize once and then restart
+        x0 = npr.rand(self.dimension)
+        res = spo.minimize(criterion, x0=x0, bounds=bounds)
+        xbest, fbest = [res[key] for key in ['x', 'fun']]
+        for _ in range(number_of_restarts):
+            # Restart strategy to avoid local behaviour of the optimizer
+            x0 = npr.rand(self.dimension)
+            res = spo.minimize(criterion, x0=x0, bounds=bounds)
+            xopt, fopt = [res[key] for key in ['x', 'fun']]
+            if fopt < fbest:
+                # the current restart has found a better point
+                xbest, fbest = xopt, fopt
+
+        return xbest
 
     def update(self, x, y):
-        """add the new point and its noisy target value
-        to the training set. Nothing to change here.
-        """
         self.X = np.concatenate((self.X, x.reshape(1,self.dimension)), 0)
         self.y = np.concatenate((self.y, y.reshape(1,1)), 0)
-        return
 
 
 #    def optimize_acquisition_criterion(self):
